5af3b3edbec3a1f6025b7884a687f67d2e8b325e
Sergio Ricardo <sergioricardo.arct@gmail.com>
2022-10-03 16:00:12 -0300

    exercicios 03

---
+++ Aula- fundamentos/Exercícios 03.py
@@ -0,0 +1,19 @@
+#data de nascimento#
+
+dia= 0o3
+mes= 0o3
+ano= 1983
+data= "Eu nasci dia %d do mês %d de %d" %(dia, mes, ano)
+print (data)
+
+#hora e minuto atual
+
+hora = 15
+min= 53
+
+print('são %d horas e %d minutos' %(hora, min))
+
+#pi
+
+pi=3.1415313216564
+print('O pi é %.4f' %(pi))
